51841e56af0fb06d864fc6795f01b7768385ed42
IvanChernec <ivancernec186@gmail.com>
2021-03-02 13:42:58 +0300

    v 1.0.2(исправлена)

--- Fone/core/src/com/mygdx/game/Actor/Enemy.java
+++ Fone/core/src/com/mygdx/game/Actor/Enemy.java
@@ -12,11 +12,11 @@ public class Enemy extends Actor1092{
     public Enemy(Texture img, Point2D position,int rank) {
         super(img, position);
 
-        switch (rank){
-            case 1: r = Main.WIDTH/250; speed = 25; score = health = 10; break;
-            case 2: r = Main.WIDTH/150; speed = 15; score = health = 20; break;
-            case 3: r = Main.WIDTH/100; speed = 10; score = health = 30; break;
-        }
+
+        if (rank == 1){ r = Main.WIDTH/250; speed = 25; score = health = 10;
+        }else if (rank == 2){ r = Main.WIDTH/150; speed = 15; score = health = 20;
+        }else if (rank == 3){ r = Main.WIDTH/100; speed = 10; score = health = 30;
+        }else { r = Main.WIDTH/50; speed = 25; score = health = 5;}
         bounds = new Circle(r, position);
         direction.setX((float) Math.sin(Math.toRadians(Math.random() * 360)));
         direction.setY((float) Math.cos(Math.toRadians(Math.random() * 360)));
